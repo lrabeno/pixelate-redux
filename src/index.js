75cd4f6752dfc468d9332884e09fad2e7b077452
Louis Rabeno <lrabeno@gmail.com>
2022-10-26 21:38:14 -0400

    react runnung

--- src/index.js
+++ src/index.js
@@ -1,44 +1,51 @@
-const table = document.getElementsByTagName('table')[0]
-const select = document.getElementsByTagName('select')[0]
-const button = document.getElementById('add-row')
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-let chosenColor = 'red'
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
 
-button.addEventListener('click', makeRow)
-table.addEventListener('click', colorize)
-table.addEventListener('mousedown', addMouseOver)
-table.addEventListener('mouseup', removeMouseOver)
-select.addEventListener('change', pickColor)
+// const table = document.getElementsByTagName('table')[0]
+// const select = document.getElementsByTagName('select')[0]
+// const button = document.getElementById('add-row')
 
-function makeRow () {
-  const row = document.createElement('tr')
-  for (let i = 0; i < 20; i++) {
-    const td = document.createElement('td')
-    row.appendChild(td)
-  }
-  table.appendChild(row)
-}
+// let chosenColor = 'red'
 
-function colorize (event) {
-  const target = event.target
-  if (target.tagName !== 'TD') {
-    return
-  }
-  if (target.className === chosenColor) {
-    target.className = ''
-  } else {
-    target.className = chosenColor
-  }
-}
+// button.addEventListener('click', makeRow)
+// table.addEventListener('click', colorize)
+// table.addEventListener('mousedown', addMouseOver)
+// table.addEventListener('mouseup', removeMouseOver)
+// select.addEventListener('change', pickColor)
 
-function pickColor (event) {
-  chosenColor = event.target.value
-}
+// function makeRow () {
+//   const row = document.createElement('tr')
+//   for (let i = 0; i < 20; i++) {
+//     const td = document.createElement('td')
+//     row.appendChild(td)
+//   }
+//   table.appendChild(row)
+// }
 
-function addMouseOver () {
-  table.addEventListener('mouseover', colorize)
-}
+// function colorize (event) {
+//   const target = event.target
+//   if (target.tagName !== 'TD') {
+//     return
+//   }
+//   if (target.className === chosenColor) {
+//     target.className = ''
+//   } else {
+//     target.className = chosenColor
+//   }
+// }
 
-function removeMouseOver () {
-  table.removeEventListener('mouseover', colorize)
-}
+// function pickColor (event) {
+//   chosenColor = event.target.value
+// }
+
+// function addMouseOver () {
+//   table.addEventListener('mouseover', colorize)
+// }
+
+// function removeMouseOver () {
+//   table.removeEventListener('mouseover', colorize)
+// }
